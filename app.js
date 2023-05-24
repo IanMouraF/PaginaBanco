d4be391c728761e6928d417c42e255dce78d6ccb
IanMouraF <ianmourafac@gmail.com>
2023-05-24 19:05:12 -0300

    tentando fazer o Entrar virar o username

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const { MongoClient } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { ObjectId } = require('mongodb');
 
 app.use(express.static(__dirname));
 
@@ -25,16 +27,42 @@ app.get('/register', (req, res) => {
   res.sendFile(path.join(__dirname, 'register.html'));
 });
 
-app.post('/register', async (req, res) => {
-  const username = req.body.username;
+// Função para gerar o token de autenticação
+function generateToken(user) {
+  // Define as informações que você deseja incluir no token (por exemplo, o ID do usuário)
+  const payload = {
+    userId: user._id.toString() // Converte o ObjectId para string
+  };
+
+  // Gera o token com uma chave secreta e um tempo de expiração (opcional)
+  const token = jwt.sign(payload, '1237689', { expiresIn: '1h' });
+
+  return token;
+}
+
+// Middleware para verificar a autenticação do usuário
+function authenticateToken(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).send('Token de autenticação não fornecido.');
+  }
+
+  jwt.verify(token, '1237689', (err, user) => {
+    if (err) {
+      return res.status(403).send('Token de autenticação inválido.');
+    }
+
+    req.user = user;
+    next();
+  });
+}
+
+// ...
+
+app.post('/login', async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-
-  const user = {
-    username,
-    email,
-    password
-  };
 
   try {
     const client = await MongoClient.connect(url, {
@@ -45,17 +73,83 @@ app.post('/register', async (req, res) => {
     const db = client.db(dbName);
     const usersCollection = db.collection('users');
 
-    await usersCollection.insertOne(user);
-    console.log('Usuário salvo com sucesso:', user);
-    res.redirect('/home');
+    const user = await usersCollection.findOne({ email, password });
+
+    if (user) {
+      console.log('Login bem-sucedido:', user);
+      const token = generateToken(user); // Gera o token de autenticação
+      await usersCollection.updateOne({ _id: user._id }, { $set: { token } }); // Armazena o token no banco de dados
+      res.redirect('/home');
+
+    } else {
+      console.log('Credenciais inválidas');
+      res.status(401).send('Credenciais inválidas');
+    }
 
     client.close();
   } catch (err) {
-    console.error('Erro ao salvar usuário:', err);
-    res.status(500).send('Erro ao salvar usuário');
+    console.error('Erro ao realizar login:', err);
+    res.status(500).send('Erro ao realizar login');
   }
 });
 
+
+
+// Rota protegida que requer autenticação
+app.get('/protected', authenticateToken, (req, res) => {
+  res.send('Informações confidenciais acessadas com sucesso!' +
+    `<script>document.getElementById("username").textContent = "${req.user.username}";</script>`);
+});
+
+
+app.post('/register', async (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const client = await MongoClient.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    const db = client.db(dbName);
+    const usersCollection = db.collection('users');
+
+    const user = await usersCollection.findOne({ email });
+
+    if (user) {
+      console.log('O usuário já está registrado:', user);
+      res.status(409).send('O usuário já está registrado');
+
+    } else {
+      const newUser = {
+        username,
+        email,
+        password
+      };
+
+      const result = await usersCollection.insertOne(newUser);
+
+      if (result.insertedId) {
+        console.log('Registro bem-sucedido:', newUser);
+        res.redirect('/login.html');
+
+      } else {
+        console.log('Erro ao registrar usuário');
+        res.status(500).send('Erro ao registrar usuário');
+      }
+    }
+
+    client.close();
+  } catch (err) {
+    console.error('Erro ao registrar usuário:', err);
+    res.status(500).send('Erro ao registrar usuário');
+  }
+});
+
+// ...
+
 app.listen(port, () => {
-  console.log('API funcionando!');
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
